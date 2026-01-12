6b48281a4b94ac3e2355a6d53c0e0f074d816cfc
Chezter Reblando <clc.reblando@gmail.com>
2026-01-13 02:05:04 +0800

    add admin and user controllers, and camera and reservation models

---
+++ Proj/Models/Camera.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proj.Models
+{
+    public class Camera
+    {
+        [Key]     
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        public string Brand { get; set; }
+        [Required]
+        public string Model { get; set; }
+        [Required]
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        [Required]
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ Proj/Models/Reservation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Proj.Models
+{
+    public class Reservation
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int CameraId { get; set; }
+        [ForeignKey("CameraId")]
+        public virtual Camera Camera { get; set; }
+        [Required]
+        public string UserId { get; set; }
+
+        [Required]
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "End Date")]
+        [DataType(DataType.Date)]
+        public DateTime EndDate { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal TotalPrice { get; set; }
+
+        [Required]
+        public string Status { get; set; }= "Pending";
+
+    }
+}
